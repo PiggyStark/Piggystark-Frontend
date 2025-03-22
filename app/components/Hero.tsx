c3fc4eafcc4cca510fade2676e320df15fbeec6b
DIVINE IFEDIORAH <ifediorahchinonso5@gmail.com>
2025-03-22 13:43:01 +0100

    hero-section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Image from 'next/image';
+import { Button } from './ui/button';
+
+const Hero = () => {
+  return (
+    <div className="bg-[#DD3C00] text-white h-screen flex items-center justify-center">
+      <main className="flex flex-row items-center justify-between text-start max-w-7xl w-full px-16">
+        {/* Left Section: Write-up */}
+        <div className="flex-1">
+          <p className="text-5xl font-bold">
+            Your Decentralized <br />
+            way to save,<br />
+            invest 🟤and HODL <br />
+          </p>
+          <p className="text-sm mt-4">
+            Automate your savings, maximize your yields, and grow your HODL effortlessly.
+          </p>
+          <div className="mt-10">
+            <Button className="bg-white text-[#DD3C00] text-lg font-bold px-6 py-3 rounded pointer">
+              Get Started
+            </Button>
+          </div>
+        </div>
+
+        {/* Right Section: Image */}
+        <div className="flex-1 flex justify-end">
+          <Image src="/mask.svg" alt="Mask Image" width={500} height={500} />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <div className="bg-[#DD3C00] h-dvh">
       <Navbar />
+      <Hero />
     </div>
   );
 }
