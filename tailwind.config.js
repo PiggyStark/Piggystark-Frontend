bbcfd6611ba3e0cb4d2a5e4ea05c18ba20ec399f
DIVINE IFEDIORAH <ifediorahchinonso5@gmail.com>
2025-03-24 22:16:28 +0100

    made corrections

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// filepath: c:\Users\Admin\Desktop\piggy\Piggystark-Frontend\tailwind.config.js
+module.exports = {
+     theme: {
+       extend: {
+         fontFamily: {
+           unbounded: ['Unbounded', 'sans-serif'],
+         },
+       },
+     },
+     plugins: [],
+   };
