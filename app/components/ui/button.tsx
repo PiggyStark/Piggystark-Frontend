d690c254757346fb2e4fc8ac7b5fb01ddbd55177
wheval <whevalmezeegbe@gmail.com>
2025-03-25 01:02:07 +0100

    fix navbar

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -5,7 +5,7 @@ const unbounded = Unbounded({ subsets: ["latin"] });
 export const Button: React.FC = () => {
   return (
     <button
-      className={`w-[200px] h-[60px] bg-white rounded-2xl text-black border-b-8 border-black ${unbounded.className} `}
+      className={`w-[200px] h-[60px] bg-white rounded-2xl text-black border-b-6 border-black ${unbounded.className} `}
     >
       Get started
     </button>
