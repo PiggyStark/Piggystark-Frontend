863a209e51a279ac6563dae229e51c8a20ba1cae
EDOHWARES <edohemmanuel4real@gmail.com>
2025-03-26 03:57:10 +0100

    simplified the code for simplicity

--- app/components/offer.tsx
+++ app/components/offer.tsx
@@ -1,85 +1,46 @@
-import React from "react";
-import Card, { CardProps } from "@components/ui/card";
-import { Unbounded } from "next/font/google";
 
+import React from "react";
+import Card from "@components/ui/card";
+import { Syne, Unbounded } from "next/font/google";
+
+const syne = Syne({ weight: ["400", "600", "700"], subsets: ["latin"] });
 const unbounded = Unbounded({ weight: ["400", "600"], subsets: ["latin"] });
 
-const cardData: CardProps[] = [
-  {
-    icon: "/images/auto.webp",
-    card: {
-      title: "Automated savings plan",
-      details:
-        "Effortlessly grow your savings with automated deposits. Choose between flexible or fixed plans and watch your crypto stack up over time",
-      width: "w-full md:w-[612px]",
-      height: "h-[466px] md:h-[566px]",
-      display: "col",
-      rounded: "22px",
-      padding: "16px",
-    },
-    img: {
-      url: "/images/Frame 45.webp",
-      size: { width: 439, height: 138.8 },
-      position: "center",
-    },
-  },
-  {
-    icon: "/images/save.webp",
-    card: {
-      title: "Allocate goal specific savings",
-      details:
-        "Save with purpose. Allocate funds to specific financial goals and track your progress—all while earning passive income.",
-      width: "w-full md:w-[612px]",
-      height: "h-[566px]",
-      display: "col",
-      rounded: "22px",
-      padding: "16px",
-    },
-    img: {
-      url: "/images/phone.webp",
-      size: { width: 339.21, height: 313.22 },
-      position: "absolute bottom-0",
-    },
-  },
-  {
-    icon: "/images/shield.webp",
-    card: {
-      title: "Long term crypto storage",
-      details:
-        "Effortlessly grow your savings with automated deposits. Choose between flexible or fixed plans and watch your crypto stack up over time",
-      width: "w-full xl:w-[1255px]",
-      height: "md:h-[528px]",
-      display: "row",
-      rounded: "22px",
-      padding: "16px",
-    },
-    img: {
-      url: "/images/storage.webp",
-      size: { width: 494.78, height: 465.29 },
-      position: "center",
-    },
-    button: true,
-  },
-];
-
-const Offer = () => {
+const Offer: React.FC = () => {
   return (
-    <div className="p-4 xl:px-16 pt-12 bg-[url('/images/bggrad.webp')] bg-cover bg-center">
-      <h2
-        className={`text-[32px] font-medium text-[#211F23] text-center mb-4 ${unbounded.className}`}
-      >
+    <div className="bg-[url('/images/bggrad.webp')] bg-cover bg-center p-8">
+      <h2 className={`text-[#211F23] font-medium text-[25px] md:text-[32px] text-center mb-[2rem] ${unbounded.className}`}>
         What we offer
       </h2>
 
-      <div className="flex flex-wrap flex-col xl:flex-row bg-transparent items-center justify-center gap-6 p-6 w-fit mx-auto">
-        {cardData.map((card, index) => (
-          <div
-            key={index}
-            className={card.card.display === "row" ? "md:col-span-2" : ""}
-          >
-            <Card {...card} />
-          </div>
-        ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-[1.3rem] gap-x-[1rem] w-fit mx-auto">
+        <Card
+          icon="/images/auto.webp"
+          title="Automated savings plan"
+          description="Effortlessly grow your savings with automated deposits. Choose between flexible or fixed plans and watch your crypto stack up over time."
+          image="/images/savings.webp"
+          extraClasses="xl:w-[612px] h-[566px]"
+          imgClasses="xl:w-[439px] xl:h-[138.8px]"
+        />
+
+        <Card
+          icon="/images/save.webp"
+          title="Allocate goal specific savings"
+          description="Save with purpose. Allocate funds to specific financial goals and track your progress—all while earning passive income."
+          image="/images/phone.webp"
+          extraClasses="xl:w-[612px] h-[566px] relative"
+          imgClasses="absolute w-[70%] sm:w-[60%] lg:w-[339.21px] xl:h-[313.22px] left-1/2 transform -translate-x-1/2 bottom-0"
+        />
+
+        <Card
+          icon="/images/shield.webp"
+          title="Long term crypto storage"
+          description="Effortlessly grow your savings with automated deposits. Choose between flexible or fixed plans and watch your crypto stack up over time."
+          image="/images/storage.webp"
+          button
+          extraClasses="md:col-span-2 h-[700px] md:h-[528px] relative"
+          imgClasses="absolute w-[65%] md:w-[40%] lg:w-auto float-end right-0 bottom-0"
+        />
       </div>
     </div>
   );
--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -1,101 +1,52 @@
-import { StaticImageData } from "next/image";
 import React from "react";
 import Image from "next/image";
-import Button from "./button";
-import clsx from "clsx";
+import Button from "@components/ui/button";
 
 import { Syne, Unbounded } from "next/font/google";
-
-export interface CardType {
-  title: string;
-  details: string;
-  width: string; // Tailwind class for width (e.g., "w-full", "md:w-1/2")
-  height: string; // Tailwind class for height (e.g., "h-64", "md:h-96")
-  display: "row" | "col";
-  rounded?: string;
-  padding?: string;
-}
-
-export interface CardProps {
-  icon: StaticImageData | string;
-  img: {
-    url: StaticImageData | string;
-    size?: { width: number; height: number };
-    position?: string;
-  };
-  card: CardType;
-  button?: boolean;
-}
-
 const syne = Syne({ weight: ["400", "600", "700"], subsets: ["latin"] });
 const unbounded = Unbounded({ weight: ["400", "600"], subsets: ["latin"] });
 
-const Card: React.FC<CardProps> = ({ icon, img, card, button }) => {
-  const isRow = card.display === "row";
 
+interface CardProps {
+  icon: string;
+  title: string;
+  description: string;
+  image: string;
+  extraClasses?: string;
+  imgClasses?: string;
+  button?: React.ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({
+  icon,
+  title,
+  description,
+  image,
+  extraClasses = "",
+  imgClasses = "",
+  button,
+}) => {
   return (
     <div
-      className={clsx(
-        "bg-white relative flex",
-        isRow
-          ? "flex-col md:flex-row justify-between"
-          : "flex-col items-center",
-        `rounded-[22px] md:rounded-[44px]`,
-        card.width,
-        card.height
-      )}
+      className={`bg-white rounded-[44px] p-[42px] xl:p-[52px] ${extraClasses}`}
     >
-      {/* Content Section */}
-      <div
-        className={clsx(
-          "flex flex-col p-8 md:p-10 lg:p-[52px] gap-4",
-          isRow ? "md:w-[50%] w-full" : "w-auto"
-        )}
-      >
-        <Image src={icon} alt={card.title} width={40} height={40} priority />
-        <h3
-          className={clsx(
-            "text-[24px] font-medium md:leading-[50px] text-black",
-            unbounded.className
-          )}
-        >
-          {card.title}
-        </h3>
-        <p
-          className={clsx(
-            "text-[16px] md:leading-[30px] font-medium text-[#000000B2] text-left",
-            syne.className
-          )}
-        >
-          {card.details}
+      <div className="space-y-[1.2rem] mb-[3rem] md:mb-[6rem]">
+        <Image src={icon} alt={title} width={40} height={39.44} />
+        <h2 className={`text-black font-medium text-[18px] md:text-[24px] ${unbounded.className}`}>{title}</h2>
+        <p className={`text-[#000000B2] font-medium text-[16px] max-w-[612px] ${syne.className}`}>
+          {description}
         </p>
-
         {button && (
-          <div className="mt-8 md:mt-16">
-            <Button text="Get started" bgColor="#FBF6FF" fontFamily={syne.className} />
+          <div className="mt-[4rem]">
+            <Button
+              bgColor="#FBF6FF"
+              fontFamily={syne.className}
+              text="Get started"
+            />
           </div>
         )}
       </div>
-
-      {/* Image Section */}
-      <div
-        className={clsx(
-          "overflow-hidden rounded-[22px] md:rounded-[44px]",
-          isRow ? "md:absolute bottom-0 right-0" : "",
-          img.position ? img.position : "",
-        )}
-      >
-        <Image
-          src={img.url}
-          alt={card.title}
-          width={img.size?.width}
-          height={img.size?.height}
-          className={clsx(
-            "object-cover w-full h-full",
-            isRow && "float-end",
-          )}
-        />
-      </div>
+      <img src={image} alt="Card image" className={imgClasses} />
     </div>
   );
 };
