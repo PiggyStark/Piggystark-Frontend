efb2fc35d0b5d856554ab2278025fe5d11027572
Prosper Williams <wprosper92@gmail.com>
2025-04-30 15:58:57 -0400

    built the landing page.

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,20 +1,19 @@
-
 interface ButtonProps {
-  text?: string;
-  bgColor?: `bg-${string}`;
-  fontFamily?: string;
+   text?: string;
+   bgColor?: `bg-${string}`;
+   fontFamily?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({ text, bgColor, fontFamily }) => {
-  return (
-    <button
-      className={`w-[200px] h-[60px] ${bgColor} rounded-2xl  text-black border-b-6 transition-all hover:border-b-0 hover:cursor-pointer hover:bg-black hover:text-white border-black ${
-        fontFamily || "font-display"
-      }`}
-    >
-      {text}
-    </button>
-  );
+export const Button: React.FC<ButtonProps> = ({text, bgColor, fontFamily}) => {
+   return (
+      <button
+         className={`w-[200px] h-[60px] ${bgColor} rounded-2xl  text-black border-b-6 transition-all hover:border-b-0 hover:cursor-pointer hover:bg-black hover:text-white border-black ${
+            fontFamily || "font-display"
+         }`}
+      >
+         {text}
+      </button>
+   );
 };
 
 export default Button;
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,15 @@ import Navbar from "./components/navbar";
 import FAQs from "./components/faqs";   // From `feat-6`
 import Offer from "@components/offer";  // From `main`
 import GetStarted from "@components/GetStarted";
+import HeroSection from "@components/HeroSection";
 
 export default function Home() {
   return (
     <>
       <div className="bg-[#DD3C00] h-dvh">
         <Navbar />
-        <FAQs />       {/* From `feat-6` */}
+        <HeroSection/>
+        {/* <FAQs />       From `feat-6` */}
       </div>
       <Offer />         {/* From `main` */}
       <Convenient />
